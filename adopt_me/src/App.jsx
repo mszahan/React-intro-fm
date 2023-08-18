63b2475111e7da202ff4de1d336d2755bc108dd2
mszahan <mszahan1@gmail.com>
2023-08-18 18:32:26 +0600

    fixed the return issue in App

--- adopt_me/src/App.jsx
+++ adopt_me/src/App.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+// import React from "react";
 import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Alex",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Doel",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Rebec",
-      breed: "Mixed",
-    }),
-  ]);
+const App = () =>{
+  return (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese"/>
+    <Pet name="Jayden" animal="cat" breed="Mixed"/>
+    <Pet name="Rebec" animal="bird" breed="Unknown"/>
+  </div>
+  );
 };
+
+
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       animal: "Dog",
+//       name: "Alex",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       animal: "Bird",
+//       name: "Pepper",
+//       breed: "Doel",
+//     }),
+//     React.createElement(Pet, {
+//       animal: "Cat",
+//       name: "Rebec",
+//       breed: "Mixed",
+//     }),
+//   ]);
+// };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
