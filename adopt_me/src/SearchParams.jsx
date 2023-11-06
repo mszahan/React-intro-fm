512a6e76bac96f84c33cd39fd7dd5799a5cd72f8
mszahan <mszahan1@gmail.com>
2023-11-06 21:04:33 +0600

    added the list of animal using hooks and map

--- adopt_me/src/SearchParams.jsx
+++ adopt_me/src/SearchParams.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 
+const ANIMAL = ['birds', 'cat', 'dog', 'rabbit', 'retptile']
+
 const SearchParams = () => {
   const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
   return (
     <div className="search-params">
       <form action="">
@@ -16,6 +19,29 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+
+        <label htmlFor='animal'>
+          Animal
+          <select 
+          id='animal'
+          value={animal}
+          onChange={e =>{
+            setAnimal(e.target.value)
+          }}>
+
+            <option/>
+            {/* here in arrow funtion the parenthesis instead of curly braces
+            .. works as the return it's called the implicit return
+            .. if you use the curly braces you will have to type return */}
+            {ANIMAL.map(animal => (
+              <option key={animal}>{animal}</option>
+            ))}
+
+          </select>
+
+        </label>
+
+
         <button>Submit</button>
       </form>
     </div>
