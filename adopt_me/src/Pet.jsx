8767c8ec8263d509fcf1d783026f652d99caadf3
mszahan <mszahan1@gmail.com>
2023-11-07 22:05:47 +0600

    updated Pet component for rendering images and other info

--- adopt_me/src/Pet.jsx
+++ adopt_me/src/Pet.jsx
@@ -1,23 +1,19 @@
-// import React from "react";
+const Pet = ({name, animal, breed, images, location, id}) => {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg'
+  if (images.length){
+    hero = images[0];
+  }
 
-// using props like self in python for data
-// props for property
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
-
-const Pet = (props) => {
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-      <hr />
-    </div>
+    <a href={`details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2> {animal} - {breed} - {location} </h2>
+      </div>
+    </a>
   );
 };
 
