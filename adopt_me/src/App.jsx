fb7758d03f777b6e0251fa8f39261df57ab596e3
mszahan <mszahan1@gmail.com>
2023-11-08 13:24:21 +0600

    used react useparams

--- adopt_me/src/App.jsx
+++ adopt_me/src/App.jsx
@@ -2,13 +2,17 @@
 import { createRoot } from 'react-dom/client';
 import SearchParams from './SearchParams.jsx';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to='/'>
+          Adopt Me!
+        </Link>
+      </header>
       <Routes>
         <Route path='details/:id' element= {<Details/>}/>
         <Route path='/' element= {<SearchParams />}/>
