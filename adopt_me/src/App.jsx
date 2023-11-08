259184065ccb0f0244d1c21257f089969eb78d4c
mszahan <mszahan1@gmail.com>
2023-11-08 13:07:32 +0600

    added the pet detail page and use react router

--- adopt_me/src/App.jsx
+++ adopt_me/src/App.jsx
@@ -2,13 +2,19 @@
 import { createRoot } from 'react-dom/client';
 import SearchParams from './SearchParams.jsx';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
+
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Routes>
+        <Route path='details/:id' element= {<Details/>}/>
+        <Route path='/' element= {<SearchParams />}/>
+      </Routes>
+    </BrowserRouter>
       
-    </div>
   );
 };
 
