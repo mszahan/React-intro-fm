e1a77c3974555405b09dfabe6936f04681fae3a3
mszahan <mszahan1@gmail.com>
2023-11-08 14:53:29 +0600

    updated the detail page and using react query

--- adopt_me/src/App.jsx
+++ adopt_me/src/App.jsx
@@ -1,13 +1,25 @@
 // import React from "react";
 import { createRoot } from 'react-dom/client';
-import SearchParams from './SearchParams.jsx';
-
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import SearchParams from './SearchParams.jsx';
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  }
+})
+
 
 const App = () => {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <header>
         <Link to='/'>
           Adopt Me!
@@ -17,6 +29,7 @@ const App = () => {
         <Route path='details/:id' element= {<Details/>}/>
         <Route path='/' element= {<SearchParams />}/>
       </Routes>
+    </QueryClientProvider>
     </BrowserRouter>
       
   );
