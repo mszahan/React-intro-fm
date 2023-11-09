6f7bdc35bfe65d8d6679e3f9eed5a3ab052f623f
mszahan <mszahan1@gmail.com>
2023-11-09 19:40:23 +0600

    activated the carousel image change link

--- adopt_me/src/Carousel.jsx
+++ adopt_me/src/Carousel.jsx
@@ -9,6 +9,12 @@ class Carousel extends Component {
         images: []
     };
 
+    handleIndexClick = event => {
+        this.setState({
+            active: +event.target.dataset.index
+        });
+    };
+
     render() {
         const {active} = this.state;
         const {images} = this.props;
@@ -17,8 +23,11 @@ class Carousel extends Component {
                 <img src={images[active]} alt="animal" />
                 <div className="carousel-smaller">
                     {images.map((photo, index) => (
+                // eslint-disable-next-line
                         <img src={photo}
-                        alt="animal thumbain"
+                        onClick={this.handleIndexClick}
+                        data-index={index}
+                        alt="animal thumbnail"
                         key={photo}
                         className={index === active ? 'active' : ''}/>
                     ))}
