1dea56ccbcb8beedd364538bd1a93d4aaf6468ff
mszahan <mszahan1@gmail.com>
2024-02-16 22:01:59 +0600

    installed tailwindcss and styled the header and searchparams with it

--- adopt_me/src/App.jsx
+++ adopt_me/src/App.jsx
@@ -1,42 +1,51 @@
 // import React from "react";
-import { useState } from "react";
+import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
 
 import SearchParams from './SearchParams.jsx';
-import Details from "./Details";
-import AdoptedPetContext from "./AdoptedPetContext";
+import Details from './Details.jsx';
+import AdoptedPetContext from './AdoptedPetContext';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
-    }
-  }
-})
-
+    },
+  },
+});
 
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-    <AdoptedPetContext.Provider value={adoptedPet}>
-      <header>
-        <Link to='/'>
-          Adopt Me!
-        </Link>
-      </header>
-      <Routes>
-        <Route path='details/:id' element= {<Details/>}/>
-        <Route path='/' element= {<SearchParams />}/>
-      </Routes>
-      </AdoptedPetContext.Provider>
-    </QueryClientProvider>
-    </BrowserRouter>
-      
+    <div
+      className="p-0 m-0"
+      style={{
+        background: 'url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)',
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header
+              className="w-full mb-10 text-center p-7 bg-gradient-to-b
+             from-yellow-400 via-orange-500 to-red-500"
+            >
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- adopt_me/src/SearchParams.jsx
+++ adopt_me/src/SearchParams.jsx
@@ -1,48 +1,48 @@
 import { useState, useContext } from 'react';
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 
 import AdoptedPetContext from './AdoptedPetContext';
-import fetchSearch from "./fetchSearch";
+import fetchSearch from './fetchSearch';
 import useBreedList from './useBreedList';
 // eslint-disable-next-line import/no-unresolved
 import Results from './Results';
 
-const ANIMAL = ['birds', 'cat', 'dog', 'rabbit', 'retptile']
+const ANIMAL = ['birds', 'cat', 'dog', 'rabbit', 'retptile'];
 
 const SearchParams = () => {
   const [requestParams, setRequestParams] = useState({
-    location: "",
-    animal: "",
-    breed: "",
+    location: '',
+    animal: '',
+    breed: '',
   });
   const [animal, setAnimal] = useState('');
-  const [breeds] = useBreedList(animal)
+  const [breeds] = useBreedList(animal);
   const [adoptedPet] = useContext(AdoptedPetContext);
 
   const results = useQuery(['search', requestParams], fetchSearch);
   const pets = results?.data?.pets ?? [];
 
-
-
   return (
-    <div className="search-params">
-      <form onSubmit={e =>{
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const obj = {
-          animal: formData.get("animal") ?? "",
-          breed: formData.get("breed") ?? "",
-          location: formData.get("location") ?? "",
-        };
-        setRequestParams(obj)
-      }}>
-        {
-          adoptedPet ? (
-            <div className="pet image-container">
-              <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
-            </div>
-          ) : null
-        }
+    <div className="my-0 mx-auto w-11/12">
+      <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col
+        justify-center items-center"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get('animal') ?? '',
+            breed: formData.get('breed') ?? '',
+            location: formData.get('location') ?? '',
+          };
+          setRequestParams(obj);
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
 
         <label htmlFor="location">
           Location
@@ -54,52 +54,45 @@ const SearchParams = () => {
           />
         </label>
 
-        <label htmlFor='animal'>
+        <label htmlFor="animal">
           Animal
-          <select 
-          id='animal'
-          name='animal'
-          onChange={(e) => {
-            setAnimal(e.target.value);
-          }}
-          onBlur={(e) => {
-            setAnimal(e.target.value);
-          }}
-         >
-
-            <option/>
+          <select
+            id="animal"
+            name="animal"
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
             {/* here in arrow funtion the parenthesis instead of curly braces
             .. works as the return it's called the implicit return
             .. if you use the curly braces you will have to type return */}
-            {ANIMAL.map(animal => (
+            {ANIMAL.map((animal) => (
               <option key={animal}>{animal}</option>
             ))}
-
           </select>
         </label>
 
-        <label htmlFor='breed'>
+        <label htmlFor="breed">
           Breed
-          <select id='breed'
-          name='breed'
-          disabled={breeds.length === 0}
-          >
-            <option/>
+          <select id="breed" name="breed" disabled={breeds.length === 0}>
+            <option />
 
-            {breeds.map(breed =>(
-              <option key={breed} value={breed}> {breed} </option>
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {' '}
+                {breed}{' '}
+              </option>
             ))}
-
           </select>
-
         </label>
-
 
         <button>Submit</button>
       </form>
-      <Results pets={pets}/>
-
-
+      <Results pets={pets} />
     </div>
   );
 };
