2b6d17e1a4bee2e0de9b044fe244e7986ecd257b
mszahan <mszahan1@gmail.com>
2024-02-27 00:23:43 +0600

    tested the api fetch call with vitest-fetch-mock

--- testing/src/__tests__/useBreedList.test.jsx
+++ testing/src/__tests__/useBreedList.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
 
@@ -24,6 +24,36 @@ test("gives an empty list with no animal", async () => {
 
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loading");
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryclient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("success"));
+
+  const [breedList] = result.current;
+  expect(breedList).toEqual(breeds);
 });
 
 // function getBreedList(animal) {
